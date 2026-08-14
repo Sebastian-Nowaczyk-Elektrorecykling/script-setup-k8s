4f1758d7ad8b5c55556dc59de33b95780b62a442
Sebastian-Nowaczyk-Elektrorecykling <139747713+Sebastian-Nowaczyk-Elektrorecykling@users.noreply.github.com>
2026-08-14 11:23:51 +0200

    Update 3-enable-addons.sh

--- 3-enable-addons.sh
+++ 3-enable-addons.sh
@@ -11,18 +11,3 @@ done
 
 section "Enable community add-on repository"
 microk8s enable community
-
-
-section "Enable CloudNativePG operator"
-if ! k get crd clusters.postgresql.cnpg.io >/dev/null 2>&1; then
-  if ! microk8s enable community:cloudnative-pg; then
-    microk8s enable cloudnative-pg
-  fi
-fi
-wait_crd clusters.postgresql.cnpg.io 900
-
-# hostpath can remain for throwaway data, but it must not remain the default once Longhorn is installed.
-if k get storageclass microk8s-hostpath >/dev/null 2>&1; then
-  k annotate storageclass microk8s-hostpath storageclass.kubernetes.io/is-default-class=false --overwrite || true
-  k annotate storageclass microk8s-hostpath storageclass.beta.kubernetes.io/is-default-class=false --overwrite || true
-fi

55969769d93f4d728ce48b607d288c3307bdf13c
Sebastian-Nowaczyk-Elektrorecykling <139747713+Sebastian-Nowaczyk-Elektrorecykling@users.noreply.github.com>
2026-08-14 11:30:53 +0200

    Update 4-install-longhorn.sh

--- 4-install-longhorn.sh
+++ 4-install-longhorn.sh
@@ -4,9 +4,13 @@ SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 source "$SCRIPT_DIR/lib/common.sh"
 require_microk8s
 
-microk8s helm repo add longhorn https://charts.longhorn.io
-microk8s helm repo update
-microk8s helm install longhorn longhorn/longhorn --namespace longhorn-system --create-namespace
+if k get storageclass longhorn >/dev/null 2>&1; then
+  log "longhorn already installed"
+else
+  microk8s helm repo add longhorn https://charts.longhorn.io
+  microk8s helm repo update
+  microk8s helm install longhorn longhorn/longhorn --namespace longhorn-system --create-namespace
+fi
 
 if k get storageclass microk8s-hostpath >/dev/null 2>&1; then
   k annotate storageclass microk8s-hostpath storageclass.kubernetes.io/is-default-class=false --overwrite || true
